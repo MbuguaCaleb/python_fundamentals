b460c3b9e0fdedb7f91fb491ea8b1d7a6e02f2b2
MbuguaCaleb <mbuguacaleb30@gmail.com>
2021-02-19 20:35:45 +0300

    tuples-and-sets

---
+++ python_sandbox_starter/04_tuples_sets.py
@@ -0,0 +1,54 @@
+# A Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+
+# Create Tuple
+fruits = ('Apples', 'Oranges', 'grapes')
+
+# Method2
+#fruits2 = tuple(('Apples', 'Oranges', 'Grapes'))
+
+# When i have only one value in a tuple i must have a trailing comma
+# Without including the comma it will be interpereted as a string
+fruits2 = ('Apples',)
+
+
+# Get Value
+print(fruits[0])
+# Just like in a list
+
+# Can't change a value
+#fruits[0] ='Pears'
+
+
+# Delete a tuple
+del fruits2
+
+# Get Length
+print(len(fruits))
+
+
+# A Set is a collection which is unordered and unindexed. No duplicate members.
+
+# Because it is unordered and unindexed it cannot allow duplicate members
+
+# It all depends on what i want to do and what i want to store
+
+# Create set
+fruits_set = {'Apples', 'Oranges', 'Mango'}
+
+# Check if in set
+# The Key word here is in
+print('Apples' in fruits_set)
+
+# Add to set
+fruits_set.add('Grape')
+
+# Remove from set
+fruits_set.remove('Grape')
+
+# clear set
+fruits_set.clear()
+
+# Delete
+del fruits_set
+
+print(fruits_set)
